492457511991de888af4398ffd761af9a5d49db9
forcewake <forcewake@gmail.com>
2014-07-24 09:32:54 +0300

    Move source code file into src folder

--- src/Functional.Maybe/Properties/AssemblyInfo.cs
+++ src/Functional.Maybe/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data.Maybe")]
+[assembly: AssemblyTitle("Functional.Maybe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bill Casarin&Andrey Tsvetkov")]
-[assembly: AssemblyProduct("Data.Maybe")]
-[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Functional.Maybe")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e8d0068-bf3f-430c-aa33-3202440fe15c")]
 
 // Version information for an assembly consists of the following four values:
 //
