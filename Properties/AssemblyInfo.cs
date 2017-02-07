0ecb881c6bdd3c8edfc7de64f63312d2b781cf9e
mike <cisephys@gmail.com>
2017-02-07 10:54:04 -0500

    Change target to .NET Standard 1.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,25 @@ using System.Reflection;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Functional.Maybe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(@"
+	Option types for C# with LINQ support and rich fluent syntax for many popular uses:
+
+	var maybeOne = ""one"".ToMaybe();
+	var maybeAnother = Maybe<string>.Nothing;
+
+	var maybeBoth = 
+		from one in maybeOne
+		from another in maybeAnother
+		select one + another;
+
+	maybeBoth.Match(
+		both =>Console.WriteLine(""Result is: {0}"", both), 
+		@else: () => Console.WriteLine(""Result is Nothing, as one of the inputs was Nothing"")
+	);")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Andrey Tsvetkov")]
+[assembly: AssemblyCompany("Andrey Tsvetkov; original version by William Casarin")]
 [assembly: AssemblyProduct("Functional.Maybe")]
-[assembly: AssemblyCopyright("MEPhI")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
